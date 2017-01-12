f853b7fbc3b77174183379e2251b871885515c01
RameshDammu20 <rameshbaba.dammu@gmail.com>
2017-01-12 13:02:26 +0530

    SecondValueController

---
+++ Temp/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Temp
+//
+//  Created by avinash pitti on 1/12/17.
+//  Copyright © 2017 avinash pitti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
